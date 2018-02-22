23a1f88d5636487711018b15b01f44bc55c35c21
Hammad Malik <hmd.node@gmail.com>
2018-02-22 13:39:52 -0500

    Prettify JSON in db/cleanedSeedData.json, remove superfluous comments from db/cleanSeedData.js

--- db/cleanSeedData.js
+++ db/cleanSeedData.js
@@ -1,25 +1,6 @@
 const fs = require('fs')
 const dirtySeedData = require('./all-entries.json')
 
-// description: String,
-// startDate: String,
-// startTime: String,
-// endDate: String,
-// endTime: String,
-// duration: String,
-// tags: Array
-
-// let timesheet = dirtySeedData[0]
-// let newTimesheet = {}
-// newTimesheet.description = timesheet.Description
-// newTimesheet.startDate = timesheet['Start date']
-// newTimesheet.startTime = timesheet['Start time']
-// newTimesheet.endDate = timesheet['End date']
-// newTimesheet.endTime = timesheet['End time']
-// newTimesheet.duraction = timesheet.Duration
-// newTimesheet.tags = timesheet.Tags.split(',').map(tag => (tag.trim()))
-// console.log(newTimesheet)
-
 let cleanedSeedData = dirtySeedData.map(timesheet => {
   let newTimesheet = {}
   newTimesheet.description = timesheet.Description
