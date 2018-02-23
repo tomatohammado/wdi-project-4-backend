070cd5b33ff7244bc720f0c780d9feffaa959ca9
Hammad Malik <hmd.node@gmail.com>
2018-02-23 10:14:50 -0500

    End exploration, find MVP and Bonus breakdown by Task

--- controller/testController.js
+++ controller/testController.js
@@ -1,84 +1,50 @@
 const Timesheet = require('../db/models/timesheet')
 const moment = require('moment')
 
-// function getWeeklyDurations (req, res) {
-//   Timesheet
-//     .find({})
-//     .then(timesheets => {
-//       let filteredTimesheets = timesheets.filter(timesheet => {
-//         return timesheet.tags.includes('wdi-week-1')
-//       })
-//       // console.log(filteredTimesheets)
-//       let weekOneOutput = {
-//         week: 'Week 1',
-//         classDuration: 0,
-//         homeworkDuration: 0,
-//         labsDuration: 0,
-//         projectDuration: 0
-//       }
-//       filteredTimesheets.forEach(timesheet => {
-//         switch (true) {
-//           case timesheet.tags.includes('wdi-class'):
-//             weekOneOutput.classDuration += moment.duration(timesheet.duration).asSeconds()
-//             break
-//           case timesheet.tags.includes('wdi-hw'):
-//             weekOneOutput.homeworkDuration += moment.duration(timesheet.duration).asSeconds()
-//             break
-//           case timesheet.tags.includes('wdi-lab'):
-//             weekOneOutput.labsDuration += moment.duration(timesheet.duration).asSeconds()
-//             break
-//           case timesheet.tags.includes('wdi-project-1', 'wdi-project-2', 'wdi-project-3', 'wdi-project-4'):
-//             weekOneOutput.projectDuration += moment.duration(timesheet.duration).asSeconds()
-//             break
-//           default:
-//             console.log('did not match class, hw, lab, or project')
-//             console.log(timesheet)
-//         }
-//       })
-
-//       weekOneOutput.classDuration = moment.duration(weekOneOutput.classDuration, 'seconds').format('hh:mm:ss')
-//       console.log(weekOneOutput.classDuration)
-//     })
-// }
-
-// getWeeklyDurations()
-
-function getTaskMvpBonus (task, taskTag) {
+function getHomeworkMvpBonusDurations (req, res) {
   Timesheet
-    .find({ tags: {$in: [taskTag]} })
+    .find({})
     .then(timesheets => {
-      let mvpDuration = 0
-      let bonusDuration = 0
-
-      timesheets.forEach(timsheet => {
-        if (timsheet.tags.includes('wdi-mvp')) {
-          mvpDuration += moment.duration(timsheet.duration).asSeconds()
-        }
-
-        if (timsheet.tags.includes('wdi-bonus')) {
-          bonusDuration += moment.duration(timsheet.duration).asSeconds()
-        }
-      })
-
-      let mvpDurationFormatted = convertSecondsToHHMMSS(mvpDuration)
-      let bonusDurationFormatted = convertSecondsToHHMMSS(bonusDuration)
-
-      // console.log(mvpDuration)
-      // console.log(bonusDuration)
-      // console.log(mvpDurationFormatted)
-      // console.log(bonusDurationFormatted)
-
-      let output = {
-        task: task,
-        tag: taskTag,
-        mvpDuration: mvpDuration,
-        bonusDuration: bonusDuration,
-        mvpDurationFormatted: mvpDurationFormatted,
-        bonusDurationFormatted: bonusDurationFormatted
-      }
-
+      let output = getTaskMvpBonus(timesheets, 'Homework', 'wdi-hw')
       console.log(output)
     })
+}
+
+function getTaskMvpBonus (timesheets, task, taskTag) {
+  let filteredTimesheets = timesheets.filter(timesheet => (
+    timesheet.tags.includes(taskTag)
+  ))
+  let mvpDuration = 0
+  let bonusDuration = 0
+
+  filteredTimesheets.forEach(timsheet => {
+    if (timsheet.tags.includes('wdi-mvp')) {
+      mvpDuration += moment.duration(timsheet.duration).asSeconds()
+    }
+
+    if (timsheet.tags.includes('wdi-bonus')) {
+      bonusDuration += moment.duration(timsheet.duration).asSeconds()
+    }
+  })
+
+  let mvpDurationFormatted = convertSecondsToHHMMSS(mvpDuration)
+  let bonusDurationFormatted = convertSecondsToHHMMSS(bonusDuration)
+
+  // console.log(mvpDuration)
+  // console.log(bonusDuration)
+  // console.log(mvpDurationFormatted)
+  // console.log(bonusDurationFormatted)
+
+  let output = {
+    task: task,
+    tag: taskTag,
+    mvpDuration: mvpDuration,
+    bonusDuration: bonusDuration,
+    mvpDurationFormatted: mvpDurationFormatted,
+    bonusDurationFormatted: bonusDurationFormatted
+  }
+
+  return output
 }
 
 function convertSecondsToHHMMSS (durationSeconds) {
@@ -94,4 +60,6 @@ function convertSecondsToHHMMSS (durationSeconds) {
 // console.log(convertSecondsToHHMMSS(167019))
 // console.log('should be: 46:23:39')
 
-getTaskMvpBonus('Homework', 'wdi-hw')
+// getTaskMvpBonus('Homework', 'wdi-hw')
+
+getHomeworkMvpBonusDurations()
